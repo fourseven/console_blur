88d73435849dc0bd266cbb0f26d6d45749d9c553
Mathew Hartley <matt@route66.sytes.net>
2013-02-26 16:26:55 +1300

    Initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+var actualCode = '(' + function () {
+  var _log = console.log;
+  console.log = function() {
+    _log.call(console, '%c' + [].slice.call(arguments).join(' '), 'color:transparent;text-shadow:0 0 2px rgba(0,0,0,.5);');
+  };
+} + ')();';
+var script = document.createElement('script');
+script.textContent = actualCode;
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
+
